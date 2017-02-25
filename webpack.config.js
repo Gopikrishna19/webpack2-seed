23c0d163a4ac36cbe2763c306100bbfaa588bce4
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-25 11:49:32 -0600

    dev only config

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,57 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Webpack = require('webpack');
 
-module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080/',
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: [
-          'babel-loader?sourceMap'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract([
-          'css-loader',
-          'sass-loader'
-        ])
-      }
+module.exports = env => {
+
+  const config = {
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: [
+            'babel-loader?sourceMap'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract([
+            'css-loader',
+            'sass-loader'
+          ])
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './src/index.html'
+      }),
+      new ExtractTextPlugin('index.css')
     ]
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './',
-    hot: true
-  },
-  plugins: [
-    new Webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './src/index.html'
-    }),
-    new ExtractTextPlugin('index.css')
-  ]
+  };
+
+  if (env && env.dev) {
+
+    config.entry.unshift('webpack-dev-server/client?http://localhost:8080/');
+    config.plugins.unshift(new Webpack.HotModuleReplacementPlugin());
+
+    config.devtool = 'source-map';
+
+    config.devServer = {
+      contentBase: './',
+      hot: true
+    };
+
+  }
+
+  return config;
+
 };
